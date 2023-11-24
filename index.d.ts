d3dc72f0fd4fb50fa28db729872c6627f63af082
Tobias <tobhei-8@student.ltu.se>
2023-11-24 23:43:42 +0100

    added suport for multiple devices

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,8 @@ export class VolumeControl {
     setVolume(volume: number): void;
     isMuted(): boolean;
     setMuted(muted: boolean);
+    getNumberOfDevices():number;
+    getDeviceName(index:number): String;
+    getDeviceId(index:number): String;
+    switchAudioDevice(id:String) : bool;
 }
