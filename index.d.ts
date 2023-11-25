077b042a7223bd43e502d74b37783da0ff2da6dd
Tobias <tobhei-8@student.ltu.se>
2023-11-25 18:08:00 +0100

    Revert "added suport for multiple devices"
    This reverts commit d3dc72f0fd4fb50fa28db729872c6627f63af082.

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,4 @@ export class VolumeControl {
     setVolume(volume: number): void;
     isMuted(): boolean;
     setMuted(muted: boolean);
-    getNumberOfDevices():number;
-    getDeviceName(index:number): String;
-    getDeviceId(index:number): String;
-    switchAudioDevice(id:String) : bool;
 }
--- src/volume_controller.cc
+++ src/volume_controller.cc
@@ -7,9 +7,6 @@
 #include <iostream>
 #include <nan.h>
 #include <wrl/client.h>
-#include <Functiondiscoverykeys_devpkey.h>
-#include <atlstr.h>
-using namespace std;
 
 using Microsoft::WRL::ComPtr;
 
@@ -20,11 +17,6 @@ std::string string_format(std::string format, Args ... args) {
   std::snprintf( buf.get(), size, format.c_str(), args ...);
   return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
 }
-struct DeviceDetails
-{
-  LPWSTR id;
-  LPWSTR name;
-};
 
 void checkErrors(HRESULT hr, std::string error_message) {
   if (FAILED(hr)) {
@@ -35,9 +27,7 @@ void checkErrors(HRESULT hr, std::string error_message) {
 class VolumeControl {
   private:
   ComPtr<IAudioEndpointVolume> device;
-  UINT numberOfDevices = 0;
-  struct DeviceDetails* dDetails;
-  ComPtr<IMMDeviceCollection> deviceList;
+
   public:
 
   VolumeControl() {
@@ -54,35 +44,6 @@ class VolumeControl {
       ),
       "Error when trying to get a handle to MMDeviceEnumerator device enumerator"
     );
-
-    checkErrors(
-      deviceEnumerator->EnumAudioEndpoints(
-        eRender,          // Audio rendering stream. Audio data flows from the application to the audio endpoint device, which renders the stream. eCapture would be the opposite
-        DEVICE_STATE_ACTIVE,         // The role that the system has assigned to an audio endpoint device. eConsole for games, system notification sounds, and voice commands
-        &deviceList   
-      ),
-      "Error when trying to get a handle to the default audio enpoint"
-    );
-    
-
-    deviceList->GetCount(&numberOfDevices);
-    dDetails = (DeviceDetails*) malloc(numberOfDevices*sizeof(DeviceDetails));
-    IMMDevice* listDevice;
-    LPWSTR id;
-    IPropertyStore* properiyStore;
-    PROPVARIANT propvariant;
-    LPWSTR pwszID = NULL;
-    PropVariantInit(&propvariant);
-    for (int i = 0; i < numberOfDevices; i++)
-    {
-      DeviceDetails d;
-      deviceList->Item(i,&listDevice);
-      (*listDevice).GetId(&d.id);
-      (*listDevice).OpenPropertyStore(STGM_READ,&properiyStore);
-      properiyStore->GetValue(PKEY_Device_FriendlyName,&propvariant);
-      d.name = propvariant.pwszVal;
-      dDetails[i] = d;
-    }
 
     // Device interface pointer where we will dig the audio device endpoint
     ComPtr<IMMDevice> defaultDevice;
@@ -105,53 +66,7 @@ class VolumeControl {
       ),
       "Error when trying to get a handle to the volume endpoint"
     );
-    
-    
-    
-    
-
   }
-
-  UINT getnumberOfDevices(){
-    return numberOfDevices;
-  }
-  string getDeviceName(UINT id){
-    return CW2A (dDetails[id].name);
-  }
-  string getDeviceId(UINT idd){
-    return CW2A (dDetails[idd].id);
-  }
-
-  bool switchAudioDevice(string Inid){
-
-    IMMDevice* listDevice;
-    LPWSTR id;
-    for (int i = 0; i < numberOfDevices; i++)
-    {
-      deviceList->Item(i,&listDevice);
-      (*listDevice).GetId(&id);
-
-
-      if( Inid.compare(CW2A(id)) == 0){
-
-        
-        device.Detach();
-
-        listDevice->Activate(            // Creates a COM object with the specified interface.
-        __uuidof(IAudioEndpointVolume),   // Reference to a GUID that identifies the interface that the caller requests be activated
-        CLSCTX_INPROC_SERVER,             // Context in which the code that manages the newly created object will run (Same process).
-        NULL,                             // Set NULL to activate the IAudioEndpointVolume endpoint https://msdn.microsoft.com/en-us/library/ms679029.aspx
-        &device                           //  Pointer to a pointer variable into which the method writes the address of the interface specified by parameter iid. Through this method, the caller obtains a counted reference to the interface.
-        );
-
-        return true;
-      }
-    }
-    return false;
-  }
-
-
-
 
   BOOL isMuted() {
     BOOL muted = false;
@@ -200,10 +115,6 @@ class VolumeControlWrapper : public Nan::ObjectWrap {
     Nan::SetPrototypeMethod(tpl, "setVolume", SetVolume);
     Nan::SetPrototypeMethod(tpl, "isMuted", IsMuted);
     Nan::SetPrototypeMethod(tpl, "setMuted", SetMuted);
-    Nan::SetPrototypeMethod(tpl, "getNumberOfDevices", getNumberOfDevices);
-    Nan::SetPrototypeMethod(tpl, "getDeviceName", getDeviceName);
-    Nan::SetPrototypeMethod(tpl, "getDeviceId", getDeviceId);
-    Nan::SetPrototypeMethod(tpl, "switchAudioDevice", switchAudioDevice);
 
     constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
     Nan::Set(target, Nan::New("VolumeControl").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
@@ -228,80 +139,6 @@ class VolumeControlWrapper : public Nan::ObjectWrap {
     }
   }
 
-  static NAN_METHOD(getNumberOfDevices) {
-    auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
-    try {
-      info.GetReturnValue().Set(obj->device.getnumberOfDevices());
-    } catch (std::string e) {
-      return Nan::ThrowError(Nan::New(e).ToLocalChecked());
-    }
-  }
-
-
-    static NAN_METHOD(getDeviceName) {
-    if (info.Length() != 1) {
-      return Nan::ThrowError(Nan::New("Exactly one number parameter is required.").ToLocalChecked());
-    }
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
-
-    int index = (int)(info[0]->NumberValue(context)).FromJust();
-    auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
-    try {
-
-      v8::Local<v8::String> myString = v8::String::NewFromUtf8(isolate,obj->device.getDeviceName(index).c_str()).ToLocalChecked();
-
-      info.GetReturnValue().Set(myString);
-    } catch (std::string e) {
-      return Nan::ThrowError(Nan::New(e).ToLocalChecked());
-    }
-    }
-
-    static NAN_METHOD(getDeviceId) {
-    if (info.Length() != 1) {
-      return Nan::ThrowError(Nan::New("Exactly one number parameter is required.").ToLocalChecked());
-    }
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
-
-    int index = (int)(info[0]->NumberValue(context)).FromJust();
-    auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
-    try {
-
-      v8::Local<v8::String> myString = v8::String::NewFromUtf8(isolate,obj->device.getDeviceId(index).c_str()).ToLocalChecked();
-
-      info.GetReturnValue().Set(myString);
-    } catch (std::string e) {
-      return Nan::ThrowError(Nan::New(e).ToLocalChecked());
-    }
-    }
-
-    static NAN_METHOD(switchAudioDevice) {
-    if (info.Length() != 1) {
-      return Nan::ThrowError(Nan::New("Exactly one number parameter is required.").ToLocalChecked());
-    }
-
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-    v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
-
-    v8::MaybeLocal<v8::String> maybeString = (info[0]->ToString(context));
-    v8::Local<v8::String> v8String = maybeString.ToLocalChecked();
-    v8::String::Utf8Value utf8Value(isolate, v8String);
-    const char* utf8String = *utf8Value;
-    auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
-    try {
-
-      //v8::Local<v8::String> myString = v8::String::NewFromUtf8(isolate,obj->device.getDeviceId(index).c_str()).ToLocalChecked();
-
-      info.GetReturnValue().Set(obj->device.switchAudioDevice(utf8String));
-    } catch (std::string e) {
-      return Nan::ThrowError(Nan::New(e).ToLocalChecked());
-    }
-    }
-
   static NAN_METHOD(GetVolume) {
     auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
     try {
@@ -315,16 +152,8 @@ class VolumeControlWrapper : public Nan::ObjectWrap {
     if (info.Length() != 1) {
       return Nan::ThrowError(Nan::New("Exactly one number parameter is required.").ToLocalChecked());
     }
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    // Create a template for the global object and set the
-    // built-in global functions.
-    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-   // global->Set(v8::String::NewFromUtf8(isolate, "log"),v8::FunctionTemplate::New(isolate, LogCallback));
-  // Each processor gets its own context so different processors
-  // do not affect each other.
-    v8::Local<v8::Context> context = v8::Context::New(isolate, nullptr, global);
 
-    double volume = (double)(info[0]->NumberValue(context)).FromJust();
+    float volume = (float)info[0]->NumberValue();
     auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
     try {
       obj->device.setVolume(volume);
@@ -346,9 +175,8 @@ class VolumeControlWrapper : public Nan::ObjectWrap {
     if (info.Length() != 1) {
       return Nan::ThrowError(Nan::New("Exactly one boolean parameter is required.").ToLocalChecked());
     }
-       
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    bool muted = (bool)(info[0]->BooleanValue(isolate));
+
+    bool muted = info[0]->BooleanValue();
     auto obj = Nan::ObjectWrap::Unwrap<VolumeControlWrapper>(info.Holder());
     try {
       obj->device.setMuted(muted);
@@ -372,8 +200,7 @@ NAN_MODULE_INIT(InitModule) {
 
   VolumeControlWrapper::Init(target);
 
-  //node::AtExit(UnInitialize);
-
+  node::AtExit(UnInitialize);
 }
 
 NODE_MODULE(addon, InitModule)
